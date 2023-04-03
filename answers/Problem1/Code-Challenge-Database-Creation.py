afbef8677652183b935cb9be472fa2e53bf5fb37
ADingankar <78924701+adingankar@users.noreply.github.com>
2023-04-03 13:48:44 -0400

    Create Code-Challenge-Database-Creation.py
    Approach:
    So I have used MySQL Database for data ingestion purpose, Amongst the options of PostgreSQL and other databases MySQL is better for memory optimization. Since there wasn't much of a large number of data size hence proceeded further with MySQL.
    
    Enhancements: Given a choice of usage of cloud service especially AWS we can store the wx_data nd yx_data in S3 and then load it to Redshift for better scalability and performance. As the time required for loading records from S3 to any other component is quite minimal.

---
+++ answers/Problem1/Code-Challenge-Database-Creation.py
@@ -0,0 +1,70 @@
+import mysql.connector as mysql
+
+#Creating the Weather Data Table:
+try:
+    mydb = mysql.connect(host="127.0.0.1", database = 'corteva_challenge_database',user="root", passwd="adina@2295")
+    # check if the connection is established
+    print(mydb.is_connected())
+
+    #Created a primary key to make the computation easier id= Stationid+date
+    query = "CREATE TABLE Weather( id VARCHAR(30) NOT NULL, Stationid VARCHAR(20) ,date DATE, \
+            max_temperature INT(10), min_temperature INT(10),precipitation INT(10), \
+            PRIMARY KEY (id))"
+
+    cursor = mydb.cursor() #create a cursor to execute queries
+    cursor.execute(query)
+    print("Weather Table Created!!")
+    mydb.close()
+except Exception as e:
+    mydb.close()
+    print(str(e))
+
+#Creating the Yeild Table :
+try:
+    mydb = mysql.connect(host="127.0.0.1", database = 'corteva_challenge_database',user="root", passwd="adina@2295")
+    # check if the connection is established
+    print(mydb.is_connected())
+
+    query = "CREATE TABLE Yield(year INT NOT NULL,corn_yield INT(10), PRIMARY KEY (year))"
+
+    cursor = mydb.cursor() 
+    cursor.execute(query)
+    print("Yeild Table Created!!")
+    mydb.close()
+except Exception as e:
+    mydb.close()
+    print(str(e))   
+
+#Creating the Logs Data Table:
+
+try:
+    mydb = mysql.connect(host="127.0.0.1", database = 'corteva_challenge_database',user="root", passwd="adina@2295")
+    # check if the connection is established
+    print(mydb.is_connected())
+
+    query = "CREATE TABLE Logs(File VARCHAR(30), start_time TIMESTAMP, end_time TIMESTAMP,\
+    num_records INT(20))"
+
+    cursor = mydb.cursor() 
+    cursor.execute(query)
+    print("Logs Table Created!!")
+    mydb.close()
+except Exception as e:
+    mydb.close()
+    print(str(e))    
+
+try:
+    mydb = mysql.connect(host="127.0.0.1", database = 'corteva_challenge_database',user="root", passwd="adina@2295")
+    # check if the connection is established
+    print(mydb.is_connected())
+
+    query = "CREATE TABLE Stastistics(year INT(4), Stationid VARCHAR(20), Average_maximum_temperature DECIMAL(20,2),\
+    Average_minimum_temperature DECIMAL(20,2), Total_accumulated_precipitation DECIMAL(20,2))"
+
+    cursor = mydb.cursor() 
+    cursor.execute(query)
+    print("Statistics Table Created!!")
+    mydb.close()
+except Exception as e:
+    mydb.close()
+    print(str(e))    
