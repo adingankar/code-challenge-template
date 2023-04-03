057bad1ce3525cf4ecd71f08a3820223bdfc2bc1
ADingankar <78924701+adingankar@users.noreply.github.com>
2023-04-03 13:54:49 -0400

    Create Weather-Data-Ingestion.py

---
+++ answers/Problem2/Weather-Data-Ingestion.py
@@ -0,0 +1,55 @@
+import os
+import pandas as pd
+import mysql.connector as mysql
+import time
+
+def current_time_stamp():
+    
+    query = "SELECT CURRENT_TIMESTAMP"
+    
+    cursor = mydb.cursor() #create a cursor to execute queries
+    cursor.execute(query)
+    time = cursor.fetchall()[0][0]
+    return time
+    
+mydb = mysql.connect(host="127.0.0.1", database = 'corteva_challenge_database',user="root", passwd="adina@2295")
+    
+folder_path = "C:\\Data-Import\\wx_data\\"
+
+for filename in os.listdir(folder_path):
+    
+    start_time = current_time_stamp()
+
+    station=filename[:-4]
+
+    weather_data_tmp = pd.read_csv(folder_path +filename, \
+                               names=['date','max_temperature','min_temperature','precipitation'],\
+                               index_col=False, delimiter = '\t', header=None)
+    weather_data_tmp['Station_id']=station
+
+    weather_data_tmp["id"]= weather_data_tmp['Station_id'].astype(str)+weather_data_tmp["date"].astype(str)
+
+    weather_data_tmp= weather_data_tmp[['id','Station_id','date','max_temperature','min_temperature','precipitation' ]]
+
+    number_records= 0
+    cursor= mydb.cursor()
+    
+    for i,row in weather_data_tmp.iterrows():
+        query = "SELECT * from Weather where id=(%s)"
+        cursor.execute(query,(row[0],))
+    
+        if cursor.fetchall()==[]:
+ 
+            query = "INSERT INTO Corteva_Challenge_Database.Weather VALUES (%s,%s,%s,%s,%s,%s)"
+            cursor.execute(query, tuple(row))
+            number_records+=1
+
+    if number_records!=0:
+        end_time = current_time_stamp()
+
+        query = "INSERT INTO Corteva_Challenge_Database.Logs VALUES (%s,%s,%s,%s)" 
+        cursor.execute(query, ("Weather_"+filename, start_time, end_time,number_records))
+
+
+    print(filename)
+    mydb.commit()
